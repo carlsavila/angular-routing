6c265436ebf00d1f63f8f75793d333bc042d860a
carlsavila <carlsavila@gmail.com>
2024-05-14 22:22:19 +0200

    First commit - Workshop 05 Les formulaires - templates driven forms - formulaire d'inscription

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { UserProfileComponent } from '../user-profile/user-profile.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { MyFormComponent } from '../my-form/my-form.component';
+import { UserFormComponent } from '../user-form/user-form.component';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, UserProfileComponent],
+  imports: [RouterLink, RouterOutlet, UserProfileComponent, UserFormComponent],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
